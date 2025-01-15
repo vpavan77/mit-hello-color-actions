da911e1f191444831774e89c6f9b3897b29bbefa
vpavan77 <vpavan77@yahoo.com>
2025-01-15 17:29:39 +0530

    commit

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello MIT CTO, time to roll up your sleeves and get your hands dirty!";
+const hello = () => "Hello MIT CTO, time to roll up your sleeves and get your hands dirty";
 console.log(hello());
 exports.hello = hello;
--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from the Office Hours in the terminal!");
+    expect(hello.hello()).toEqual("Hello MIT CTO, time to roll up your sleeves and get your hands dirty");
   });
 });
